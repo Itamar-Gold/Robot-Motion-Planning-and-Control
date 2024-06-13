631fb6b81c2872c9b231950405ea985e62b7e6d5
Itamar-Gold <itamargoldschmidt@.gmail.com>
2024-06-13 13:35:36 +0300

    setup

--- kinematics.py
+++ kinematics.py
@@ -1,7 +1,6 @@
 import numpy as np
 
 
-def forward_kinematics(thetas: [float], links) -> [float]:
     """
 
     :param thetas:
@@ -10,15 +9,7 @@ def forward_kinematics(thetas: [float], links) -> [float]:
     """
     state = []
 
-    r1 = [[np.cos(thetas[1]), - np.sin(thetas[1]), 0],
-          [np.sin(thetas[1]), np.cos(thetas[1]), 0],
-          [0, 0, 1]]
 
-    r2 = [[np.cos(thetas[2]), - np.sin(thetas[2]), 0],
-          [np.sin(thetas[2]), np.cos(thetas[2]), 0],
-          [0, 0, 1]]
 
-    for i in thetas:
-        state[i] = np.matmul(r1, r2)
 
 def inverse_kinematics(thetas: [float]) -> None:
