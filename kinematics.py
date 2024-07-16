c6d5205af29968504630232223c568c3b9754dfe
“[Yoav <[Yoav_Mann@Pickommerce.com]>
2024-07-16 19:05:47 +0300

    setup

--- kinematics.py
+++ kinematics.py
@@ -1,8 +1,7 @@
 import numpy as np
 import sympy as sp
 
-
-def forward_kinematics(thetas: [float], links: [float], thetad: [float], thetadd: [float]) -> [float]:
+def forward_kinematics(thetas: [float], links: [float], mass: [float], thetad: [float], thetadd: [float]) -> [float]:
     """
     :param thetas:
     :param links:
@@ -41,29 +40,6 @@ def forward_kinematics(thetas: [float], links: [float], thetad: [float], thetadd
         [0, 0, 0, 1]
     ])
 
-    # Matrixes for Mass Center calculation
-
-    r12mass = np.array([
-        [1, 0, 0, 0],
-        [0, np.cos(thetas[1]), - np.sin(thetas[1]), 0],
-        [0, np.sin(thetas[1]), np.cos(thetas[1]), 0.5 * links[0]],
-        [0, 0, 0, 1]
-    ])
-
-    r23mass = np.array([
-        [1, 0, 0, 0],
-        [0, np.cos(thetas[2]), -np.sin(thetas[2]), 0.5 * links[1]],
-        [0, np.sin(thetas[2]), np.cos(thetas[2]), 0],
-        [0, 0, 0, 1]
-    ])
-
-    r34mass = np.array([
-        [1, 0, 0, 0],
-        [0, 1, 0, 0.5 * links[2]],
-        [0, 0, 1, 0],
-        [0, 0, 0, 1]
-    ])
-
     # Calculated Matrixes
 
     r02 = np.matmul(r01, r12)
@@ -74,91 +50,191 @@ def forward_kinematics(thetas: [float], links: [float], thetad: [float], thetadd
     p = r04[0:3, 3:]
 
     # Jacobian Matrix
-    # Prismatic Vector
-    v1 = np.array([0, 0, 1])
+    r01diff = np.array([
+        [1, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1]
+    ])
 
-    # Row 1-3, Column 1
-    j1 = np.array([
+    r12diff = np.array([
+        [-np.sin(thetas[0]), 0, np.cos(thetas[0]), 0],
+        [0, 1, 0, 0],
+        [-np.cos(thetas[0]), 0, -np.sin(thetas[0]), 0],
+        [0, 0, 0, 1]
+    ])
+
+    r23diff = np.array([
+        [-np.sin(thetas[1]), -np.cos(thetas[1]), 0, links[1] * -np.sin(thetas[1])],
+        [np.cos(thetas[1]), -np.sin(thetas[1]), 0, links[1] * np.cos(thetas[1])],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1]
+    ])
+
+    r23diff3 = np.array([
+        [np.cos(thetas[1]), -np.sin(thetas[1]), 0, 0],
+        [np.sin(thetas[1]), np.cos(thetas[1]), 0, 0],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1]
+    ])
+
+    r34diff = np.array([
+        [-np.sin(thetas[2]), -np.cos(thetas[2]), 0, links[2] * -np.sin(thetas[2])],
+        [np.cos(thetas[2]), -np.sin(thetas[2]), 0, links[2] * np.cos(thetas[2])],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1]
+    ])
+
+    r02diff1 = np.matmul(r01, r12diff)
+    r03diff1 = np.matmul(r02diff1, r23)
+    r04diff1 = np.matmul(r03diff1, r34)
+
+    r02diff2 = np.matmul(r01diff, r12)
+    r03diff2 = np.matmul(r02diff2, r23diff)
+    r04diff2 = np.matmul(r03diff2, r34)
+
+    r03diff3 = np.matmul(r02diff2, r23diff3)
+    r04diff3 = np.matmul(r03diff3, r34diff)
+
+    jp = np.array([
+        [r04diff1[0, 3], r04diff2[0, 3], r04diff3[0, 3]],
+        [r04diff1[1, 3], r04diff2[1, 3], r04diff3[1, 3]],
+        [r04diff1[2, 3], r04diff2[2, 3], r04diff3[2, 3]]
+    ])
+
+    print(f'jp: ', jp)
+    print(f'r04diff3: ', r04diff3)
+    print(f'r34diff3: ', r34diff)
+    print(f'r03: ', r03)
+
+    # Center of mass calculations
+    r01mass = np.array([
+        [1, 0, 0, 0],
+        [0, 1, 0, 0.5 * links[0]],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1]
+    ])
+
+    r23mass = np.array([
+        [np.cos(thetas[1]), -np.sin(thetas[1]), 0, 0.5 * links[1] * np.cos(thetas[1])],
+        [np.sin(thetas[1]), np.cos(thetas[1]), 0, 0.5 * links[1] * np.sin(thetas[1])],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1]
+    ])
+
+    r34mass = np.array([
+        [np.cos(thetas[2]), -np.sin(thetas[2]), 0, 0.5 * links[2] * np.cos(thetas[2])],
+        [np.sin(thetas[2]), np.cos(thetas[2]), 0, 0.5 * links[2] * np.sin(thetas[2])],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1]
+    ])
+
+    # Jv calculations
+    r23massdiff = np.array([
+        [-np.sin(thetas[1]), -np.cos(thetas[1]), 0, 0.5 * links[1] * -np.sin(thetas[1])],
+        [np.cos(thetas[1]), -np.sin(thetas[1]), 0, 0.5 * links[1] * np.cos(thetas[1])],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1]
+    ])
+
+    r34massdiff = np.array([
+        [-np.sin(thetas[2]), -np.cos(thetas[2]), 0, links[2] * -np.sin(thetas[2])],
+        [np.cos(thetas[2]), -np.sin(thetas[2]), 0, links[2] * np.cos(thetas[2])],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1]
+    ])
+
+    # Jv1
+    r02massdiff1 = np.matmul(r01mass, r12diff)
+
+    jv1 = np.array([
+        [r02massdiff1[0, 3], 0, 0],
+        [r02massdiff1[1, 3], 0, 0],
+        [r02massdiff1[2, 3], 0, 0]
+    ])
+
+    jv1trans = jv1.transpose()
+
+    # JV2
+    r02diff1 = np.matmul(r01, r12diff)
+    r03diff1 = np.matmul(r02diff1, r23mass)
+
+    r02diff2 = np.matmul(r01diff, r12)
+    r03diff2 = np.matmul(r02diff2, r23massdiff)
+
+    jv2 = np.array([
+        [r03diff1[0, 3], r03diff2[0, 3], 0],
+        [r03diff1[1, 3], r03diff2[1, 3], 0],
+        [r03diff1[2, 3], r03diff2[2, 3], 0]
+    ])
+
+    jv2trans = jv2.transpose()
+
+    # JV3
+    r02diff1 = np.matmul(r01, r12diff)
+    r03diff1 = np.matmul(r02diff1, r23)
+    r04diff1 = np.matmul(r03diff1, r34mass)
+
+    r02diff2 = np.matmul(r01diff, r12)
+    r03diff2 = np.matmul(r02diff2, r23diff)
+    r04diff2 = np.matmul(r03diff2, r34mass)
+
+    r03diff3 = np.matmul(r02diff2, r23diff3)
+    r04diff3 = np.matmul(r03diff3, r34massdiff)
+
+    jv3 = np.array([
+        [r04diff1[0, 3], r04diff2[0, 3], r04diff3[0, 3]],
+        [r04diff1[1, 3], r04diff2[1, 3], r04diff3[1, 3]],
+        [r04diff1[2, 3], r04diff2[2, 3], r04diff3[2, 3]]
+    ])
+
+    jv3trans = jv3.transpose()
+
+    # JW
+    jw1 = np.array([
+        [0, 0, 0],
         [1, 0, 0],
-        [0, 1, 0],
-        [0, 0, 1]
+        [0, 0, 0]
     ])
 
-    j1 = np.matmul(v1, j1)
-    j1 = np.array([
-        [j1[0]],
-        [j1[1]],
-        [j1[2]]
+    jw1trans = jw1.transpose()
+
+    jw2 = np.array([
+        [0, np.sin(thetas[0]), 0],
+        [1, 0, 0],
+        [0, np.cos(thetas[0]), 0]
     ])
 
-    # Row 1-3, Column 2
-    j2a = r01[0:3, 0:3]
-    j2b2 = r01[0:3, 3:]
-    j2a = np.matmul(v1, j2a)
+    jw2trans = jw2.transpose()
 
-    j2b1 = r03[0:3, 3:]
-
-    j2b = j2b1 - j2b2
-
-    j2 = np.array([
-        (j2a[1] * j2b[2]) - (j2a[2] * j2b[1]),
-        (j2a[2] * j2b[0]) - (j2a[0] * j2b[2]),
-        (j2a[0] * j2b[1]) - (j2a[1] * j2b[0])
+    jw3 = np.array([
+        [0, np.sin(thetas[0]), np.sin(thetas[0])],
+        [1, 0, 0],
+        [0, np.cos(thetas[0]), np.cos(thetas[0])]
     ])
 
-    # Row 1-3 Column 3
-    j3a = r02[0:3, 0:3]
-    j3b2 = r02[0:3, 3:]
-    j3a = np.dot(v1, j3a)
+    jw3trans = jw3.transpose()
 
-    j3b1 = r03[0:3, 3:]
+    # Inertia model
 
-    j3b = j3b1 - j3b2
-
-    j3 = np.array([
-        (j3a[1] * j3b[2]) - (j3a[2] * j3b[1]),
-        (j3a[2] * j3b[0]) - (j3a[0] * j3b[2]),
-        (j3a[0] * j3b[1]) - (j3a[1] * j3b[0])
+    i = np.array([
+        [(mass[0]*links[0]**2)/12, 0, 0],
+        [0, (mass[1]*links[1]**2)/12, 0],
+        [0, 0, (mass[2]*links[2]**2)/12]
     ])
 
-    # Rotation / Orientation Vector
+    m1 = mass[0]*jv1trans*jv1 + mass[1]*jv2trans*jv2 + mass[2]*jv2trans*jv2
+    m2 = jw1trans*i*jw1 + jw2trans*i*jw2 + jw3trans*i*jw3
+    m = m1 + m2
 
-    # Row 4-6 Column 1
-    j4 = np.array([
-        [0],
-        [0],
-        [0]
-    ])
+    g1 = - jv1trans*9.81*mass[0]
+    g2 = - jv2trans*9.81*mass[1]
+    g3 = - jv3trans*9.81*mass[2]
+    g = g1 + g2 + g3
 
-    # Row 4-6, Column 2
-    j5 = np.array([
-        [j2a[0]],
-        [j2a[1]],
-        [j2a[2]]
-    ])
-
-    # Row 4-6, Column 3
-    j6 = np.array([
-        [j3a[0]],
-        [j3a[1]],
-        [j3a[2]]
-    ])
-
-    # Creating the Jacobian Matrix
-    jm1 = np.concatenate((j1, j2, j3), 1)
-    jm2 = np.concatenate((j4, j5, j6), 1)
-    j = np.concatenate((jm1, jm2), 0)
-    jp = j[0:3, 0:3]
-
-    pdot = np.matmul(jp, thetad)
-    pdubeldot = pdot + np.matmul(jp, thetadd)
-
-    # Debugging
-    print(f'pos: ', p)
-    print(f'pos speed: ', pdot)
-    print(f'pos exaleration: ', pdubeldot)
-
-    return p, pdot, pdubeldot
+    print(f'p: ', p)
+    print(f'm: ', m)
+    print(f'g: ', g)
 
 
 def inverse_kinematics(position: [float], links: [float]) -> [float]:
